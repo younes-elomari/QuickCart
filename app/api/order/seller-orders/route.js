75e4941b919dcf991585744d880683102deaddd5
younes-elomari <youneselaomari@gmail.com>
2025-07-29 04:24:08 -0700

    finished project

--- app/api/order/seller-orders/route.js
+++ app/api/order/seller-orders/route.js
@@ -1,21 +1,21 @@
 import connectDB from "@/config/db";
 import Address from "@/models/Address";
 import Order from "@/models/Order";
-import Product from "@/models/Product";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
   try {
     const { userId } = getAuth(request);
+    const isSeller = await authSeller(userId);
+    if (!isSeller) {
+      return NextResponse.json({ success: false, message: "Not Authorized" });
+    }
     await connectDB();
+
     Address.length;
-    Product.length;
 
-    const orders = await Order.find({ userId }).populate(
-      "address items.product"
-    );
-
+    const orders = await Order.find({}).populate("address items.product");
     return NextResponse.json({ success: true, orders });
   } catch (error) {
     return NextResponse.json({ success: false, message: error.message });
